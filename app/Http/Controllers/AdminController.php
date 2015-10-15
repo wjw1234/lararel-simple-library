98b17f015c3e9dc10fd9a47efab34aa8af35a5c8
Elliott Landsborough <elliott.landsborough@codegent.com>
2015-10-16 00:29:49 +0100

    Admin functions

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,11 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Book;
+
 class AdminController extends Controller
 {
 
+    protected $books;
+
+    public function __construct()
+    {
+        $this->books = Book::get();
+    }
+
     public function home() {
-        return view('admin.home');
+        return view('admin.home', ['books'=>$this->books]);
+    }
+
+    public function book($id) {
+        return view('admin.book', ['currentbook'=>$id,'books'=>$this->books]);
+    }
+
+    public function settings() {
+    	return view('admin.settings', ['page'=>'settings','books'=>$this->books]);
     }
 
 }
