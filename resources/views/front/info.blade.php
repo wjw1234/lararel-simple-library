2831409bac318a405a0c3623410fd76f8ef4aee1
Elliott Landsborough <elliott.landsborough@codegent.com>
2015-10-20 22:20:20 +0100

    Info page

--- resources/views/front/info.blade.php
+++ resources/views/front/info.blade.php
@@ -2,8 +2,35 @@
 
 @section('content')
 
-{!! $settings->email !!}
-{!! $settings->phone !!}
-{!! $settings->video_url !!}
+<p>For portfolio requests and further information please contact:</p>
+
+@if (strlen($settings->address))
+<p>
+	{!! nl2br($settings->address) !!}
+</p>
+@endif
+
+@if (strlen($settings->phone))
+<p>
+	TEL: <a href="tel:{!! $settings->phone !!}">{!! $settings->phone !!}</a>
+</p>
+@endif
+
+@if (strlen($settings->fax))
+<p>
+	FAX: <a href="tel:{!! $settings->phone !!}">{!! $settings->fax !!}</a>
+</p>
+@endif
+
+@if (strlen($settings->email))
+<p>
+	{!! $cleanEmail	!!}
+</p>
+@endif
+
+<p>
+	<a href="http://www.clmuk.com" target="_blank">www.clmuk.com</a>
+</p>
+
 
 @endsection
