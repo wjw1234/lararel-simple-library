cd5206e49fe436909c3bfcfb5fe4dd1e523af678
Elliott Landsborough <elliott.landsborough@codegent.com>
2015-10-20 22:19:19 +0100

    Obfuscate email

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -9,6 +9,19 @@ class FrontController extends Controller
 {
 	protected $books;
 
+	private function hide_email($email) {
+    	if (!strlen($email)) return false;
+    	$character_set = '+-.0123456789@ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz';
+	  	$key = str_shuffle($character_set); $cipher_text = ''; $id = 'e'.rand(1,999999999);
+  		for ($i=0;$i<strlen($email);$i+=1) $cipher_text.= $key[strpos($character_set,$email[$i])];
+		$script = 'var a="'.$key.'";var b=a.split("").sort().join("");var c="'.$cipher_text.'";var d="";';
+		$script.= 'for(var e=0;e<c.length;e++)d+=b.charAt(a.indexOf(c.charAt(e)));';
+  		$script.= 'document.getElementById("'.$id.'").innerHTML="<a href=\\"mailto:"+d+"\\">"+d+"</a>"';
+  		$script = "eval(\"".str_replace(array("\\",'"'),array("\\\\",'\"'), $script)."\")"; 
+  		$script = '<script type="text/javascript">/*<![CDATA[*/'.$script.'/*]]>*/</script>';
+  		return '<span id="'.$id.'">[javascript protected email address]</span>'.$script;
+	}
+
     public function home() {
     	$settings = Setting::first();
     	$books = Book::get();
@@ -26,9 +39,7 @@ class FrontController extends Controller
     public function info() {
     	$settings = Setting::first();
     	$books = Book::get();
-    	return view('front.info', ['books'=>$books, 'settings'=>$settings]);
+    	return view('front.info', ['books'=>$books, 'settings'=>$settings, 'cleanEmail'=>Self::hide_email($settings->email)]);
     }
-
-
 
 }
