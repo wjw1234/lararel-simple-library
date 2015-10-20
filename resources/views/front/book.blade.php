a37ece867d06139c22b6d134fc977bbd90764bdd
Elliott Landsborough <elliott.landsborough@codegent.com>
2015-10-20 22:20:26 +0100

    Cook carousel

--- resources/views/front/book.blade.php
+++ resources/views/front/book.blade.php
@@ -2,10 +2,29 @@
 
 @section('content')
 
-	@foreach ($currentbook->slides as $slide)
-        @foreach ($slide->images as $image)
-            <img src="{!! url('/images/uploaded/'.$image->file->file) !!}" />
-        @endforeach
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+  	<?php $i = 0; ?>
+  	@foreach ($currentbook->slides as $slide)	
+  		<div class="item {!! !$i ? 'active' : null !!}">
+        	@foreach ($slide->images as $image)
+            	<img src="{!! url('/images/uploaded/'.$image->file->file) !!}" />
+        	@endforeach
+        </div>
+        <?php $i++; ?>
     @endforeach
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 
 @endsection
