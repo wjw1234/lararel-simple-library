594cfeb6021617d0173aefc3f349659ae3b230cb
Elliott Landsborough <elliott.landsborough@codegent.com>
2015-10-20 22:19:42 +0100

    Add fontawesome and some columns

--- resources/views/front.blade.php
+++ resources/views/front.blade.php
@@ -9,18 +9,45 @@
         <title></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Lato:400,300,100">
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="{{elixir("css/app.css")}}">
     </head>
-    <body>
+    <body class="main">
 
-<ul>
-    @foreach ($books as $book)
-        <li class="{!! isset($currentbook) && $currentbook->id == $book->id ? 'active' : null !!}"><a href="/book/{!! $book->id  !!}">{!! $book->title !!}</a></li>
-    @endforeach
-</ul>
+        <div class="container">
 
-        @yield('content')
+        <div class="col-md-3 col-sm-3">
+            @if (strlen($settings->site_title))
+                <h1>{!! $settings->site_title !!}</h1>
+            @endif
+            <ul class="menu">
+                <li>Stories
+                    <ul>
+                        @foreach ($books as $book)
+                            <li class="{!! isset($currentbook) && $currentbook->id == $book->id ? 'active' : null !!}"><a href="/book/{!! $book->id  !!}">{!! $book->title !!}</a></li>
+                        @endforeach
+                    </ul>
+                </li>
+                @if (strlen($settings->video_url))
+                    <li><a href="{!! $settings->video_url !!}" target="_blank">Video</a></li>
+                @endif
+                <li><a href="/info">Info + Contact</a></li>
+            </ul>
+            <div class="social">
+                @if (strlen($settings->instagram))
+                    <a class="instagram" target="_blank" href="{!! $settings->instagram !!}"><i class="fa fa-instagram"></i></a>
+                @endif
+                @if (strlen($settings->tumblr))
+                    <a class="tumblr" target="_blank" href="{!! $settings->tumblr !!}"><i class="fa fa-tumblr-square"></i></a>
+                @endif
+            </div>
+        </div>
+
+        <div class="col-md-9 col-sm-9 content">
+            @yield('content')
+        </div>
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
@@ -36,3 +63,9 @@
         </script>
     </body>
 </html>
+
+
+
+
+
+<!-- github.com/ElliottLandsborough -->
