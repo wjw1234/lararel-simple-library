29331ecdbc80c3376359933a17bc92c9f06236d7
Elliott Landsborough <elliott.landsborough@codegent.com>
2015-10-20 22:20:36 +0100

    Format settings page

--- resources/views/admin/settings.blade.php
+++ resources/views/admin/settings.blade.php
@@ -10,10 +10,24 @@
 
     {!! BootForm::open()->action(url('/dashboard/settings')) !!}
     {!! BootForm::bind($settings) !!}
-        {!! BootForm::text('Video Url', 'video_url')->placeholder('http://video.com') !!}
-        {!! BootForm::text('Phone', 'phone')->placeholder('07986 543 364') !!}
-        {!! BootForm::text('Email Address', 'email')->placeholder('example@domain.com') !!}
-        {!! BootForm::submit('Save')->class("btn") !!}
+
+        <div class="container">
+
+            <div class="col-md-6">
+                {!! BootForm::text('Title', 'site_title')->placeholder('John Smith') !!}
+                {!! BootForm::text('Video Url', 'video_url')->placeholder('http://video.com') !!}
+                {!! BootForm::text('Phone', 'phone')->placeholder('07986 543 364') !!}
+                {!! BootForm::text('Fax', 'fax')->placeholder('07986 543 364') !!}
+                {!! BootForm::text('Email Address', 'email')->placeholder('example@domain.com') !!}
+                
+                {!! BootForm::submit('Save')->class("btn btn-primary") !!}
+            </div>
+            <div class="col-md-6">
+                {!! BootForm::text('Intstagram Link', 'instagram')->placeholder('http://instagram.com/jsmith') !!}
+                {!! BootForm::text('Tumblr Link', 'tumblr')->placeholder('http://tumblr.com/j.smith') !!}
+                {!! BootForm::textarea('Address', 'address')->placeholder('123 Address Street') !!}            
+            </div>
+        </div>
     {!! BootForm::close() !!}
 
     </div>
