f0d685644487327f9fd2a70da4ffa3900852e1ef
Elliott Landsborough <elliott.landsborough@gmail.com>
2016-04-12 22:24:58 +0100

    Fix forms

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
       <!-- Example row of columns -->
       <div class="row">
         <div class="col-md-4 col-md-offset-4">
-            <form role="form" method="POST" action="/auth/login">
+            <form role="form" method="POST" action="{!! url('/auth/login') !!}">
                 {!! csrf_field() !!}
                 <div class="form-group">
                     <input type="email" name="email" placeholder="Email" class="form-control" value="{!! old('email') !!}">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,32 +1,28 @@
 @extends('admin')
 @section('content')
 
-<form method="POST" action="/auth/register">
-    {!! csrf_field() !!}
-
-    <div>
-        Name
-        <input type="text" name="name" value="{!! old('name') !!}">
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+            <form role="form" method="POST" action="{!! url('/auth/register') !!}">
+                {!! csrf_field() !!}
+                <div class="form-group">
+                    <input type="text" placeholder="name" name="name" value="{!! old('name') !!}">
+                </div>
+                <div class="form-group">
+                    <input type="email" placeholder="email" name="email" value="{!! old('email') !!}">
+                </div>
+                <div class="form-group">
+                    <input type="password" placeholder="password" name="password">
+                </div>
+                <div class="form-group">
+                    <input type="password" placeholder="confirm password" name="password_confirmation">
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-success">Register</button>
+                </div>
+            </form>
+        </div>
     </div>
-
-    <div>
-        Email
-        <input type="email" name="email" value="{!! old('email') !!}">
-    </div>
-
-    <div>
-        Password
-        <input type="password" name="password">
-    </div>
-
-    <div>
-        Confirm Password
-        <input type="password" name="password_confirmation">
-    </div>
-
-    <div>
-        <button type="submit">Register</button>
-    </div>
-</form>
-
+</div>
 @endsection
